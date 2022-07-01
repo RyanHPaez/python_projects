f178d5137c7e661d3b1d575fa6fe7d2e7395bab6
RyanHPaez <solosnail@gmail.com>
2022-06-30 18:59:29 -0700

    project

---
+++ functions_practice.py
@@ -0,0 +1,20 @@
+def hello(name):
+  print("Hello "+ name)
+
+hello('whatever your name is.')
+
+# ---------------------------------------------
+pack_set= {'pack1', 'pack2', 'pack3'}
+print(pack_set)
+
+
+# ---------------------------------------------
+
+def my_function(fruit, meal, drink):
+  print("First I eat "+fruit+" Next I eat "+meal+" Then if your thirsty drink "+drink+" My lunchbox is empty!")
+
+my_function("Strawberries,", "yogurt,", "water,")
+
+
+
+
