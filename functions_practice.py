3da64a236f28a9da3e4a369fdbbfcb0501b09217
RyanHPaez <solosnail@gmail.com>
2022-07-01 11:47:51 -0700

    making it look nice

--- functions_practice.py
+++ functions_practice.py
@@ -11,7 +11,7 @@ print(pack_set)
 # ---------------------------------------------
 
 def my_function(fruit, meal, drink):
-  print("First I eat "+fruit+" Next I eat "+meal+" Then if your thirsty drink "+drink+" My lunchbox is empty!")
+  print("I get hungry at 12, I usually have a pre-packed meal it includes: \n\tA "+fruit+"\n\tNext I eat "+meal+"\n\tThen if I'm your thirsty I drink "+drink+"\n\tMy lunchbox is empty!")
 
 my_function("Strawberries,", "yogurt,", "water,")
 
