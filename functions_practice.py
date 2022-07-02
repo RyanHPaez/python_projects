b7e2331528e446876282bb6b6d03892a05311db2
RyanHPaez <solosnail@gmail.com>
2022-07-02 09:44:26 -0700

    forgot to define functions

--- functions_practice.py
+++ functions_practice.py
@@ -1,26 +1,22 @@
 def hello(name):
-    print("Hello " + name)
-
+  print("Hello "+ name)
 
 hello('whatever your name is.')
 
 # ---------------------------------------------
-
-
 def pack_set(pack1, pack2, pack3):
-    print(pack1+pack2+pack3)
-
-
-pack_set("\ta", "\tb", "\tc")
-pack_set("\nalpha", "\nbeta", "\ncharlie")
+  print(pack1+pack2+pack3)
+  
+pack_set ("\ta","\tb","\tc")
+pack_set ("\nalpha","\nbeta","\ncharlie")
 
 # ---------------------------------------------
 
-
 def eat_lunch(fruit, meal, drink):
-    print("I get hungry at 1pm, I usually have a pre-packed meal, which includes: \n\tSome "+fruit +
-          ".\n\tNext I eat "+meal+".\n\tThen if I'm thirsty I drink "+drink+".\n\tAfter that my lunchbox is empty!")
-
+  print("I get hungry at 1pm, I usually have a pre-packed meal, which includes: \n\tSome "+fruit+".\n\tNext I eat "+meal+".\n\tThen if I'm thirsty I drink "+drink+".\n\tAfter that my lunchbox is empty!")
 
 eat_lunch("Strawberries", "yogurt", "water")
 eat_lunch("apples", "sandwich", "juice")
+
+
+
