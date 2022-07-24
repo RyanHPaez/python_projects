1b8426c7df4cc14cadade22b497d4f3403883c0e
RyanHPaez <solosnail@gmail.com>
2022-07-24 13:39:26 -0700

    more learned codes

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+# pip3 install numpy
 import numpy as np
 
 a = np.array([1,2,3,4])
---
+++ 3.py
@@ -0,0 +1,12 @@
+# pip3 install numpy
+import numpy as np
+
+a = np.array([1,2,3,4])
+
+def multiply_elements_by_3(arr):
+    return 3 * arr
+
+multiply_elements_by_3(a)
+
+print (multiply_elements_by_3(a))
+
